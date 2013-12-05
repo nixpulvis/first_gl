732b3d92af840d884be4a732343d8029c5cfb7d1
Nathan Lilienthal <nathan@nixpulvis.com>
2013-12-05 00:57:13 -0500

    Make compiling on OSX work.

--- game.c
+++ game.c
@@ -1,9 +1,12 @@
-// #include <windows.h>
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-
-#include <stdio.h>
+#endif
 
 typedef struct {
   float x;
